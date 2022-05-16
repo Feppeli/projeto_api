d86730e93ed91b5092aad0bcfc6f76fe8d0d885b
Feppeli <dfsilvat@gmail.com>
2022-05-16 18:40:37 -0300

    add functions

--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
+const bancoDeDados = require('../DB')
+
 const port = 3003
 
 const express = require('express')
 const app = express()
 
 app.get('/produtos', (req, res, next) => {
-    console.log('Middleware 1...')
-    next()
+    res.send(bancoDeDados.getProdutos())
+
 })
 
-app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 4230.00}) // conversão automática ao JSON
+app.get('./produts/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(request.params.id))
+})
 
+app.post('/produtos', (req,res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
 })
 
 app.listen(port, () => {
