41991f7423f8764bf6a4a74797f0b430e7663c07
Feppeli <dfsilvat@gmail.com>
2022-05-16 16:59:05 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const port = 3003
+
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, next) => {
+    console.log('Middleware 1...')
+    next()
+})
+
+app.get('/produtos', (req, res, next) => {
+    res.send({nome: 'Notebook', preco: 4230.00}) // conversão automática ao JSON
+
+})
+
+app.listen(port, () => {
+    console.log(`Servidor executando na porta ${port}.`)
+})
