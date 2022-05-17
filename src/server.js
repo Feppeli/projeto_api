f65e2c0a99e89b7ee223c714e67ad56ed3c04d64
Feppeli <dfsilvat@gmail.com>
2022-05-17 16:39:12 -0300

    finish

--- src/server.js
+++ src/server.js
@@ -1,27 +1,43 @@
-const bancoDeDados = require('../DB')
+const porta = 3003;
 
-const port = 3003
-
-const express = require('express')
+const express = require('express');
 const app = express()
+const bodyParser = require('../node_modules/body-parser')
+const bancoDeDados = require('./DB')
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos())
-
 })
 
-app.get('./produts/:id', (req, res, next) => {
-    res.send(bancoDeDados.getProduto(request.params.id))
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
 })
 
-app.post('/produtos', (req,res, next) => {
+app.post('/produtos', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id:req.params.id,
+        nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto) //JSON
 })
 
-app.listen(port, () => {
-    console.log(`Servidor executando na porta ${port}.`)
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
 })
